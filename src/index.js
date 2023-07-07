daecec561816cc8abc24fcada71623bf88c1c788
joelalcala <joel@joelalcala.net>
2023-07-07 18:14:10 -0400

    track function cleanup

--- src/index.js
+++ src/index.js
@@ -3,56 +3,86 @@ import abbreviationsUtil from './utils/abbreviations.js';
 import dataStore from './store.js';
 import dataFetchers from './fetchers.js';
 const { abbreviations } = config;
+const hasSessionCookie = dataStore.hasSessionCookie();
+const hasFirstVisitCookie = dataStore.hasFirstVisitCookie();
 
 const audubonTracker = (function () {
+
+  function getPerPageData() {
+    const perPageData = {
+      clickPath: dataFetchers.clickPath(),
+      cta: dataFetchers.cta()
+    };
+    return perPageData;
+  }
+
+  function getPerSessionData() {
+    const perSessionData = {
+      browser: dataFetchers.browser(),
+      pagePath: dataFetchers.pagePath(),
+      subdomain: dataFetchers.subdomain(),
+      urlParams: dataFetchers.urlParams(),
+      referrer: dataFetchers.referrer(),
+      device: dataFetchers.device(),
+      uniqueVisitorId: dataFetchers.uniqueVisitorId(),
+    }
+    return perSessionData;
+  }
+
+  function getPerFirstVisitData() {
+    const perFirstVisitData = {
+      firstVisitDate: dataFetchers.firstVisitDate()
+    }
+    return perFirstVisitData;
+  }
+
+  function getSessionCount() {
+    let sessionCount = 1;
+    if (hasFirstVisitCookie && dataStore.firstVisitData.sc) {
+      sessionCount = dataStore.firstVisitData.sc + 1;
+    }
+    return sessionCount;
+  }
 
   const track = async () => {
     dataStore.initialize();
 
-    const hasSessionCookie = dataStore.hasSessionCookie();
-    const hasFirstVisitCookie = dataStore.hasFirstVisitCookie();
-    
-    const clickPath = dataFetchers.clickPath();
-    const cta = dataFetchers.cta();
+    let perSessionData = {}
+    let perFirstVisitData = {}
 
+    // Set data collected on each page
+    let perPageData = getPerPageData();
+    const { clickPath, cta } = perPageData;
     if (clickPath || cta) {
-      const sessionData = {};
-
-      if (clickPath) {
-        sessionData.clickPath = clickPath;
-      }
-      if (cta) {
-        sessionData.cta = cta;
-      }
-
-      dataStore.setSessionData(sessionData);
+      dataStore.setSessionData(perPageData);
     }
 
-    if (!hasSessionCookie) {
-      let sessionCount = 1;
-      if (hasFirstVisitCookie && dataStore.firstVisitData.sc) {
-        sessionCount = dataStore.firstVisitData.sc + 1;
-      }
-      const sessionData = {
-        sessionCount: sessionCount,
-        browser: dataFetchers.browser(),
-        pagePath: dataFetchers.pagePath(),
-        subdomain: dataFetchers.subdomain(),
-        urlParams: dataFetchers.urlParams(),
-        referrer: dataFetchers.referrer(),
-        device: dataFetchers.device(),
-        firstVisitDate: dataFetchers.firstVisitDate(),
-        uniqueVisitorId: dataFetchers.uniqueVisitorId(),
-      };
 
-      dataStore.setFirstVisitData(sessionData);
+    if (!hasSessionCookie) {
+      const sessionCount = getSessionCount();
+      perSessionData.sessionCount = sessionCount;
+      perSessionData = {...getPerSessionData(), ...perSessionData};
+      dataStore.setSessionData(perSessionData);
+
+      if (!hasFirstVisitCookie) {
+        perFirstVisitData = {...getPerFirstVisitData(), ...perSessionData};
+        dataStore.setFirstVisitData(perFirstVisitData);
+      } else{
+        perFirstVisitData = dataStore.firstVisitData;
+        perFirstVisitData.sessionCount = sessionCount;
+        dataStore.setFirstVisitData(perFirstVisitData);
+      }
 
       const ipAddress = await dataFetchers.ipAddress();
       if (ipAddress) {
-        sessionData.ipAddress = ipAddress;
-        dataStore.setSessionData(sessionData);
-        dataStore.firstVisitData.ipAddress = ipAddress;
-        dataStore.setFirstVisitData(dataStore.firstVisitData);
+        perSessionData.ipAddress = ipAddress;
+        dataStore.setSessionData(perSessionData);
+
+        if (!hasFirstVisitCookie) {
+          perFirstVisitData.ipAddress = ipAddress;
+          dataStore.setFirstVisitData(perFirstVisitData);
+        }
+
       }
     }
   };
