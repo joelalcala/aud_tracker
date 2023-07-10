29ce76867a4497195d9501c110f85ec25dc6de15
joelalcala <joel@joelalcala.net>
2023-07-10 17:06:56 -0400

    testing both first visit and session cookies

--- test/aud_tracker.test.js
+++ test/aud_tracker.test.js
@@ -44,38 +44,51 @@ describe('Audubon Tracker', () => {
     });
 
     test('landingPage is correct', async () => {
-      const landingPage = await page.evaluate('audubonTracker.getSession("landingPage")');
-      expect(landingPage).toBe('/field-guide/bird/magnificent-frigatebird');
-    });    
+      const svLandingPage = await page.evaluate('audubonTracker.getSession("landingPage")');
+      const fvLandingPage = await page.evaluate('audubonTracker.getFirstVisit("landingPage")');
+      expect(svLandingPage).toBe('/field-guide/bird/magnificent-frigatebird');
+      expect(fvLandingPage).toBe('/field-guide/bird/magnificent-frigatebird');
+    });
 
     test('subdomain is correct', async () => {
-      const subdomain = await page.evaluate('audubonTracker.getSession("subdomain")');
-      expect(subdomain).toBe('www');
+      const svSubdomain = await page.evaluate('audubonTracker.getSession("subdomain")');
+      const fvSubdomain = await page.evaluate('audubonTracker.getFirstVisit("subdomain")');
+      expect(svSubdomain).toBe('www');
+      expect(fvSubdomain).toBe('www');
     });
 
     test('sessionCount is correct', async () => {
-      const sessionCount = await page.evaluate('audubonTracker.getFirstVisit("sessionCount")');
-      expect(sessionCount).toBe(1);
+      const fvSessionCount = await page.evaluate('audubonTracker.getFirstVisit("sessionCount")');
+      expect(fvSessionCount).toBe(1);
     });
 
     test('utm_source is correct', async () => {
-      const utmSource = await page.evaluate('audubonTracker.getSession("utm_source")');
-      expect(utmSource).toBe('facebook');
+      const fvUtmSource = await page.evaluate('audubonTracker.getFirstVisit("utm_source")');
+      const svUtmSource = await page.evaluate('audubonTracker.getSession("utm_source")');
+      expect(fvUtmSource).toBe('facebook');
+      expect(svUtmSource).toBe('facebook');
     });
     
     test('utm_medium is correct', async () => {
-      const utmMedium = await page.evaluate('audubonTracker.getSession("utm_medium")');
-      expect(utmMedium).toBe('social');
+      const fvUtmMedium = await page.evaluate('audubonTracker.getFirstVisit("utm_medium")');
+      const svUtmMedium = await page.evaluate('audubonTracker.getSession("utm_medium")');
+      expect(fvUtmMedium).toBe('social');
+      expect(svUtmMedium).toBe('social');
     });
     
     test('utm_campaign is correct', async () => {
-      const utmCampaign = await page.evaluate('audubonTracker.getSession("utm_campaign")');
-      expect(utmCampaign).toBe('20230600_nas_eng');
+      const fvUtmCampaign = await page.evaluate('audubonTracker.getFirstVisit("utm_campaign")');
+      const svUtmCampaign = await page.evaluate('audubonTracker.getSession("utm_campaign")');
+      expect(fvUtmCampaign).toBe('20230600_nas_eng');
+      expect(svUtmCampaign).toBe('20230600_nas_eng');
     });
 
     test('ms is correct', async () => {
-      const ms = await page.evaluate('audubonTracker.getSession("ms")');
-      expect(ms).toBe('digital-eng-social-facebook-x-20230600-nas_eng');
+      const fvMs = await page.evaluate('audubonTracker.getFirstVisit("ms")');
+      const svMs = await page.evaluate('audubonTracker.getSession("ms")');
+      expect(fvMs).toBe('digital-eng-social-facebook-x-20230600-nas_eng');
+      expect(svMs).toBe('digital-eng-social-facebook-x-20230600-nas_eng');
+
     });
 
     test('firstVisitDate is correct', async () => {
@@ -84,18 +97,24 @@ describe('Audubon Tracker', () => {
     });
 
     test('ipAddress is present', async () => {
-      const ipAddress = await page.evaluate('audubonTracker.getSession("ipAddress")');
-      expect(ipAddress).toBeTruthy();
+      const fvIpAddress = await page.evaluate('audubonTracker.getFirstVisit("ipAddress")');
+      const svIpAddress = await page.evaluate('audubonTracker.getSession("ipAddress")');
+      expect(fvIpAddress).toBeTruthy();
+      expect(svIpAddress).toBeTruthy();
     });
 
     test('browser is correct', async () => {
-      const browser = await page.evaluate('audubonTracker.getSession("browser")');
-      expect(browser).toBe('Chrome');
+      const fvBrowser = await page.evaluate('audubonTracker.getFirstVisit("browser")');
+      const svBrowser = await page.evaluate('audubonTracker.getSession("browser")');
+      expect(fvBrowser).toBe('Chrome');
+      expect(svBrowser).toBe('Chrome');
     });
 
     test('referrer is correct', async () => {
-      const referrer = await page.evaluate('audubonTracker.getSession("referrer")');
-      expect(referrer).toBe(REFERRER);
+      const fvReferrer = await page.evaluate('audubonTracker.getFirstVisit("referrer")');
+      const svReferrer = await page.evaluate('audubonTracker.getSession("referrer")');
+      expect(fvReferrer).toBe(REFERRER);
+      expect(svReferrer).toBe(REFERRER);
     });
 
     test('uniqueVisitorId is present', async () => {
